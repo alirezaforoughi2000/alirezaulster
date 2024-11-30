ba501dbad40c516736b2d29720124ff3265ee2f9
alirezaforoughi2000 <foroughialireza2000@gmail.com>
2024-11-30 15:19:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Basic JavaScript functionality for interactivity
+document.querySelectorAll('.sidebar ul li').forEach((item) => {
+  item.addEventListener('click', () => alert('This feature is under development!'));
+});
+
+document.querySelector('.login-btn').addEventListener('click', () => {
+  alert('Log in functionality will be implemented here.');
+});
