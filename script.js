c7afe30931f1b0b487faa7c4dc84223cd6532d12
alirezaforoughi2000 <foroughialireza2000@gmail.com>
2024-11-30 15:26:08 +0000

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// Basic JavaScript functionality for interactivity
-document.querySelectorAll('.sidebar ul li').forEach((item) => {
-  item.addEventListener('click', () => alert('This feature is under development!'));
-});
-
-document.querySelector('.login-btn').addEventListener('click', () => {
-  alert('Log in functionality will be implemented here.');
-});
